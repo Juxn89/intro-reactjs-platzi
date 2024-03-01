0d3135dd4cd6e232ee3f1bd06824e4f6f535ed46
Juan Gómez <gomezj01@stepstone.com>
2024-03-01 09:35:00 -0600

    LocalStorage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import {
   TodoSearch,
   TodoCounter
 } from './components/';
-import { defaultTodos } from './data/todos'
+import { defaultTodos } from './data/todos';
 import './App.css';
 
 function App() {
-	const [todos, setTodos] = useState(defaultTodos);
+	const locaStorageToDo = localStorage.getItem('ToDo_v1')
+	let parsedTodos = [];
+
+	if(locaStorageToDo === null) {
+		localStorage.setItem('ToDo_v1', JSON.stringify(defaultTodos))
+		parsedTodos = defaultTodos
+	}
+	else
+		parsedTodos = JSON.parse(locaStorageToDo)
+
+	const [todos, setTodos] = useState(parsedTodos);
 	const [searchValue, setSearchValue] = useState('');
 
 	const total = todos.length
 	const totalCompleted = todos.filter(todos => !!todos.completed).length
+
+	const saveToDo = (newTodos) => {
+		const jsonTodos = JSON.stringify(newTodos)
+		localStorage.setItem('ToDo_v1', jsonTodos)
+		setTodos(newTodos)
+	}
 
 	const searchedTodos = todos.filter(todo => 
 		todo.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
@@ -25,13 +41,13 @@ function App() {
 		const todoToUpdateIndex = updatedListToDos.findIndex(todo => todo.id === id)
 		updatedListToDos[todoToUpdateIndex].completed = true;
 
-		setTodos(updatedListToDos)
+		saveToDo(updatedListToDos)
 	}
 
 	const deleteToDo = (id) => {
 		const updatedListToDos = todos.filter(todo => todo.id !== id);
 
-		setTodos(updatedListToDos)
+		saveToDo(updatedListToDos)
 	}
 
   return (
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -5,13 +5,13 @@ import styles from './TodoItem.module.css'
 export function TodoItem(props) {
   const { name, completed, handleComplete, handleDetele } = props;
 
-	return(
-		<li className={ styles.TodoItem }>
-			<CompleteIcon completed={ completed } onComplete={ handleComplete } />
+return(
+	<li className={ styles.TodoItem }>
+		<CompleteIcon completed={ completed } onComplete={ handleComplete } />
 
-			<p className={ `${styles['TodoItem-p']} ${completed && styles['TodoItem-p--complete']}` }>{name}</p>
+		<p className={ `${styles['TodoItem-p']} ${completed && styles['TodoItem-p--complete']}` }>{name}</p>
 
-			<DeleteIcon onDelete={ handleDetele } />
-		</li>
-	);
+		<DeleteIcon onDelete={ handleDetele } />
+	</li>
+);
 }
